c0fec4adf2dc62008a07eb246c151657208a2665
RuichangBao <1294838346@qq.com>
2023-02-20 16:00:12 +0800

    C#反射

---
+++ Reflection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            string className = "Reflection.Student";
+            Type type = Type.GetType(className);
+            dynamic student = type.Assembly.CreateInstance(className, true, BindingFlags.Default, null, new object[] { 1000 }, null, null);
+            //Student student = new Student(10086);
+            //Type type = student.GetType();
+
+            FieldInfo fieldInfo = type.GetField("id", BindingFlags.NonPublic | BindingFlags.Instance);
+            object value1 = fieldInfo.GetValue(student);
+            Console.WriteLine(value1.GetType());
+            int num = (int)fieldInfo.GetValue(student);
+            Console.WriteLine(value1.ToString());
+
+
+            MethodInfo methodInfo = type.GetMethod("Print", BindingFlags.NonPublic | BindingFlags.Instance);
+            methodInfo.Invoke(student, null);
+            Console.WriteLine(methodInfo.GetType());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Reflection/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    class Student
+    {
+        private int id;
+        public Student(int id)
+        {
+            this.id = id;
+        }
+
+        private void Print()
+        {
+            Console.WriteLine("私有输出方法" + id);
+        }
+    }
+}
