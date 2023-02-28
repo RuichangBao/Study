8323a20ece1d73cea4f3bed229107c3124f11854
RuichangBao <1294838346@qq.com>
2023-02-28 11:46:57 +0800

    aaa

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -8,18 +8,17 @@ namespace Reflection
         static void  Main(string[] args)
         {
             string className = "Reflection.Student";
+            //反射创建对象
             Type type = Type.GetType(className);
             dynamic student = type.Assembly.CreateInstance(className, true, BindingFlags.Default, null, new object[] { 1000 }, null, null);
-            //Student student = new Student(10086);
-            //Type type = student.GetType();
 
             FieldInfo fieldInfo = type.GetField("id", BindingFlags.NonPublic | BindingFlags.Instance);
+            //通过反射访问私有变量
             object value1 = fieldInfo.GetValue(student);
             Console.WriteLine(value1.GetType());
-            int num = (int)fieldInfo.GetValue(student);
             Console.WriteLine(value1.ToString());
 
-
+            //通过反射访问私有方法
             MethodInfo methodInfo = type.GetMethod("Print", BindingFlags.NonPublic | BindingFlags.Instance);
             methodInfo.Invoke(student, null);
             Console.WriteLine(methodInfo.GetType());
