0613d4b3a490a90648ddec7996eed311add970d6
RuichangBao <1294838346@qq.com>
2023-07-21 18:22:31 +0800

    版本变更

---
+++ ScreenSize/Assets/AutoBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoBackground : MonoBehaviour
+{
+    [HideInInspector]
+    public float wideHeightRate;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ScreenSize/Assets/AutoBackgroundInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AutoBackground))]
+public class AutoBackgroundInspector : Editor
+{
+    private AutoBackground autoBackground;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        autoBackground = this.target as AutoBackground;
+        autoBackground.wideHeightRate = EditorGUILayout.Slider("宽高比:",autoBackground.wideHeightRate, 0, 5);
+        Application.isEditor
+    }
+}
