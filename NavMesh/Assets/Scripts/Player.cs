231b5325f1af10b40bf9a927845e7c1f05edcda3
RuichangBao <1294838346@qq.com>
2023-09-14 16:07:29 +0800

    navMehs寻找最近的点

--- NavMesh/Assets/Scripts/Player.cs
+++ NavMesh/Assets/Scripts/Player.cs
@@ -5,15 +5,26 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    public NavMeshAgent navMeshAgent;
     public Transform target;
+    public Transform samplePos;
+    private NavMeshAgent navMeshAgent;
     void Start()
     {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if (NavMesh.SamplePosition(samplePos.position,out NavMeshHit hit,5,1))
+        {
+            Debug.LogError("找到最近的点");
+            navMeshAgent.SetDestination(hit.position); 
+        }
+        else
+        {
+            Debug.LogError("未找到最近的点");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position); //每帧更新目标位
+        //navMeshAgent.SetDestination(target.position); //每帧更新目标位
     }
 }
