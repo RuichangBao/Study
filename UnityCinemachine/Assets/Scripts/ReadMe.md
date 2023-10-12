a73019d204401c676b5ddd5cd6980dbbd03c0c0f
RuichangBao <1294838346@qq.com>
2023-10-12 21:25:31 +0800

    aaa

--- UnityCinemachine/Assets/Scripts/ReadMe.md
+++ UnityCinemachine/Assets/Scripts/ReadMe.md
@@ -1,2 +1,3 @@
 Unity 虚拟相机学习
 参考 https://blog.csdn.net/LLLLL__/article/details/127214113
+https://blog.csdn.net/weixin_39766005/article/details/107223623
--- UnityCinemachine/Assets/Scripts/Test.cs
+++ UnityCinemachine/Assets/Scripts/Test.cs
@@ -1,15 +1,32 @@
 using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    public CinemachineVirtualCamera cinemachineVirtualCamera;
-    public CinemachineSmoothPath cinemachineSmoothPath;
+    public CinemachineBrain cinemachineBrain;
+    public Button Btn;
+    public CinemachineVirtualCamera[] cinemachineVirtualCameras;
+    private int index = 0;
+
+
     void Start()
     {
-        cinemachineVirtualCamera.enabled = true;
+        Btn.onClick.AddListener(BtnOnClick);
+        cinemachineBrain.m_CameraCutEvent = TestAAA();
+    }
 
+ 
+
+    private void BtnOnClick()
+    {
+        index++;
+        index %= 2;
+        cinemachineVirtualCameras[index].Priority += 20;
+        //cinemachineBrain.re
     }
 }
