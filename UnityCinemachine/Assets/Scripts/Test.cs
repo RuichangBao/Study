cac8b225214826226ce1456b5875b0d973405d10
RuichangBao <1294838346@qq.com>
2023-10-13 15:13:21 +0800

    虚拟相机

--- UnityCinemachine/Assets/Scripts/Test.cs
+++ UnityCinemachine/Assets/Scripts/Test.cs
@@ -1,32 +1,22 @@
 using Cinemachine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    public CinemachineBrain cinemachineBrain;
     public Button Btn;
-    public CinemachineVirtualCamera[] cinemachineVirtualCameras;
-    private int index = 0;
-
+    public CinemachineImpulseSource cinemachineImpulse;
 
     void Start()
     {
+        //Debug.LogError
         Btn.onClick.AddListener(BtnOnClick);
-        cinemachineBrain.m_CameraCutEvent = TestAAA();
     }
 
  
 
     private void BtnOnClick()
     {
-        index++;
-        index %= 2;
-        cinemachineVirtualCameras[index].Priority += 20;
-        //cinemachineBrain.re
+        cinemachineImpulse.GenerateImpulse();
     }
 }
